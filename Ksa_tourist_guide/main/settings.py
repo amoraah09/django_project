64adf7bac098008c787ad6fc31d40f4243c1197b
amoraah09 <159419693+amoraah09@users.noreply.github.com>
2024-03-15 22:58:56 +0000

    move styles to static styles file , and use image to display hero image

--- Ksa_tourist_guide/main/settings.py
+++ Ksa_tourist_guide/main/settings.py
@@ -55,6 +55,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, "apps" + os.sep + "templates")
 
 
 
+
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
@@ -119,10 +120,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
-STATICFILES_DIRS = [
-    BASE_DIR / "static"
-]
+
+MEDIA_URL = '/images/'
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
