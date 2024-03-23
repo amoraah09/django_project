c05cb072c13fd45bfdabed0412f0dc4cc3da906f
amoraah09 <159419693+amoraah09@users.noreply.github.com>
2024-03-23 22:17:15 +0000

    add model Spot

--- Ksa_tourist_guide/apps/TouristSpots/models.py
+++ Ksa_tourist_guide/apps/TouristSpots/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Spot(models.Model):
+    name = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    description = models.TextField()
+    location = models.CharField(max_length=200)
